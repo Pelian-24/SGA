41eaa13e7e6e3f188781028d3afd0818c670e5e4
Pelian-24 <gregoretelian518@gmail.com>
2026-09-02 14:44:04 -0300

    Configuraciones nuevas, repositorio nuevo copia de seguridad

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+const alumnosRoutes = require("./routes/alumnos.routes")
+app.use("/alumnos", alumnosRoutes)
+
+let alumnos = [
+    {
+        id: 1,
+        nombre: "Ana",
+        carrera: "Programación"
+    },
+    {
+        id: 2,
+        nombre: "José",
+        carrera: "Sistemas"
+    }
+]
+// Creo un middleware
+app.use((req, res, next) => {
+    console.log(req.method)
+    console.log(req.url)
+    next()
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Servidor funcionando en http://localhost:3000")
+})
